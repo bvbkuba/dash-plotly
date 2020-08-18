37c7692557659487761a84713d39bf4115a2df19
break <makikd@op.pl>
2020-08-18 17:34:58 +0200

    Simple label with text input

--- projects/callback_1.py
+++ projects/callback_1.py
@@ -8,9 +8,16 @@ app = dash.Dash(__name__)
 
 app.layout = html.Div([
     dcc.Input(id='input_1', value='', type='text'),
-    html.Div(id='div_1',children='')
+    html.Div(id='div_1',children=''),
 
 ])
+@app.callback(
+    Output(component_id='div_1',component_property='children'),
+    [Input(component_id='input_1',component_property='value')]
 
+)
+
+def update_div(text_input):
+    return f"Wprowadzono: {text_input}"
 if __name__ == '__main__':
     app.run_server(debug=True, port=8051, dev_tools_hot_reload=True)
