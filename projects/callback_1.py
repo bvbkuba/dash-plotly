ac08f2647c415dc7fc40956730fb309edf0650bf
break <makikd@op.pl>
2020-08-18 17:03:08 +0200

    Deleted file and create new file with imports

---
+++ projects/callback_1.py
@@ -0,0 +1,11 @@
+import dash
+import dash_core_components as dcc
+import dash_html_components as html
+import plotly.graph_objects as go
+
+app = dash.Dash(__name__)
+
+app.layout =
+
+if __name__ == '__main__':
+    app.run_server(debug=True, port=8051, dev_tools_hot_reload=True)
