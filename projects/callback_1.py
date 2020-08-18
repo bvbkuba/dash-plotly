cd82df3f814492df7339935345ba8e66aea04b7f
break <makikd@op.pl>
2020-08-18 17:07:53 +0200

    Start's project

--- projects/callback_1.py
+++ projects/callback_1.py
@@ -2,10 +2,15 @@ import dash
 import dash_core_components as dcc
 import dash_html_components as html
 import plotly.graph_objects as go
+from dash.dependencies import Input,Output
 
 app = dash.Dash(__name__)
 
-app.layout =
+app.layout = html.Div([
+    dcc.Input(id='input_1', value='', type='text'),
+    html.Div(id='div_1',children='')
+
+])
 
 if __name__ == '__main__':
     app.run_server(debug=True, port=8051, dev_tools_hot_reload=True)
